ad1c3db934104e012575cd7cd1e1e0ec37ff762a
James Pancoast <jpancoast@gmail.com>
2014-11-17 21:07:21 -0700

    blah blah blah

--- docs/gen_diagram.sh
+++ docs/gen_diagram.sh
@@ -2,4 +2,5 @@
 
 
 #/opt/local/bin/dot -Tpng -o overall_layout.png -Gnewrank overall_layout.dot
-/opt/local/bin/dot -Tpng -o overall_layout.png overall_layout.dot
+#/opt/local/bin/dot -Tpng -o overall_layout.png overall_layout.dot
+ccomps -x overall_layout.dot| dot | gvpack -array3 | neato -Tpng -n2 -o overall_layout.png
